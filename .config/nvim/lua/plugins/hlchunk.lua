325b9bd8cb92c94f17f749d8b5f2e35f85382afb
leokang94 <acccdang8659@gmail.com>
2025-01-12 01:29:06 +0900

    imp(nvim): update hlchunk.nvim, bump plugins

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -5,43 +5,43 @@ return {
 		local util = require("dracula.util")
 
 		local function get_blank_bg_color(color)
-			return util.blend_bg(color, 0.115)
+			return util.blend_bg(color, 0.1)
 		end
 
 		require("hlchunk").setup({
 			chunk = {
 				enable = true,
-				delay = 100,
-				style = { { fg = vim.g.color.purple, bold = true } },
+				delay = 0, -- no animation
+				style = { { fg = vim.g.color.pink, bold = true } },
 				chars = {
 					horizontal_line = "─",
 					vertical_line = "│",
-					left_top = "┌",
-					left_bottom = "└",
+					left_top = "╭",
+					left_bottom = "╰",
 					right_arrow = "─",
 				},
 			},
-			indent = {
-				enable = true,
-				chars = {
-					"│",
-					"¦",
-					"┆",
-					"┊",
-				},
-			},
-			-- blank = {
+			-- indent = {
 			-- 	enable = true,
 			-- 	chars = {
-			-- 		" ",
-			-- 	},
-			-- 	style = {
-			-- 		{ bg = get_blank_bg_color(vim.g.color.green) },
-			-- 		{ bg = get_blank_bg_color(vim.g.color.yellow) },
-			-- 		{ bg = get_blank_bg_color(vim.g.color.pink) },
-			-- 		{ bg = get_blank_bg_color(vim.g.color.white) },
+			-- 		"│",
+			-- 		"¦",
+			-- 		"┆",
+			-- 		"┊",
 			-- 	},
 			-- },
+			blank = {
+				enable = true,
+				chars = {
+					" ",
+				},
+				style = {
+					{ bg = get_blank_bg_color(vim.g.color.green) },
+					{ bg = get_blank_bg_color(vim.g.color.yellow) },
+					{ bg = get_blank_bg_color(vim.g.color.pink) },
+					{ bg = get_blank_bg_color(vim.g.color.white) },
+				},
+			},
 		})
 	end,
 }
