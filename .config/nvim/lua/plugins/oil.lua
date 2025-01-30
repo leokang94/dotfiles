62780297f46d1ad08ccd0873b3207be144dec257
leokang94 <acccdang8659@gmail.com>
2025-01-30 14:37:39 +0900

    imp(nvim): update oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,27 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+
+		keymaps = {
+			-- ["g?"] = { "actions.show_help", mode = "n" },
+			-- ["<CR>"] = "actions.select",
+			["<M-h>"] = { "actions.parent", mode = "n" },
+			["<M-l>"] = "actions.select",
+			["<M-v>"] = { "actions.select", opts = { vertical = true } },
+			["<M-s>"] = { "actions.select", opts = { horizontal = true } },
+			["<M-t>"] = { "actions.select", opts = { tab = true } },
+			-- ["<C-p>"] = "actions.preview",
+			-- ["<C-c>"] = { "actions.close", mode = "n" },
+			-- ["<C-l>"] = "actions.refresh",
+			-- ["-"] = { "actions.parent", mode = "n" },
+			-- ["_"] = { "actions.open_cwd", mode = "n" },
+			-- ["`"] = { "actions.cd", mode = "n" },
+			-- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+			-- ["gs"] = { "actions.change_sort", mode = "n" },
+			-- ["gx"] = "actions.open_external",
+			-- ["g."] = { "actions.toggle_hidden", mode = "n" },
+			-- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+		},
 	},
 	keys = {
 		{
