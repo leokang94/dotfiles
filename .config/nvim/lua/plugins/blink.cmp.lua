51d7dffa0f659137ec42652eb1ff5248ec82b203
leokang94 <acccdang8659@gmail.com>
2025-02-17 05:40:13 +0900

    imp(nvim): auto complete cmdline with blink.cmp

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -113,6 +113,7 @@ return {
         menu = {
           border = "single",
           scrollbar = false,
+          auto_show = true,
           draw = {
             columns = { { "item_idx" }, { "kind_icon" }, { "label", "label_description", gap = 1 } },
             components = {
@@ -247,6 +248,31 @@ return {
             border = "single",
           },
         },
+      },
+
+      cmdline = {
+        enabled = true,
+        keymap = nil, -- Inherits from top level `keymap` config when not set
+        sources = function()
+          local type = vim.fn.getcmdtype()
+          -- Search forward and backward
+          if type == '/' or type == '?' then return { 'buffer' } end
+          -- Commands
+          if type == ':' or type == '@' then return { 'cmdline' } end
+          return {}
+        end,
+        completion = {
+          trigger = {
+            show_on_blocked_trigger_characters = {},
+            show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+          },
+          menu = {
+            auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
+            draw = {
+              columns = { { 'label', 'label_description', gap = 1 } },
+            },
+          }
+        }
       },
 
       signature = {
