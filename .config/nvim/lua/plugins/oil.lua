b6e76795532904b49a764588be60d590fec69a88
leokang94 <acccdang8659@gmail.com>
2025-02-17 14:10:08 +0900

    imp(nvim): update oil keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,53 +1,54 @@
 local is_vsplit = require("utils.screen").is_vsplit
 
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		default_file_explorer = true,
-		columns = { "icon", "size" },
-		watch_for_chagnes = true,
-		view_options = {
-			show_hidden = true,
-		},
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explorer = true,
+    columns = { "icon", "size" },
+    watch_for_chagnes = true,
+    view_options = {
+      show_hidden = true,
+    },
 
-		keymaps = {
-			-- ["g?"] = { "actions.show_help", mode = "n" },
-			-- ["<CR>"] = "actions.select",
-			["<M-h>"] = { "actions.parent", mode = "n" },
-			["<M-l>"] = "actions.select",
-			["<M-v>"] = { "actions.select", opts = { vertical = true } },
-			["<M-s>"] = { "actions.select", opts = { horizontal = true } },
-			["<M-t>"] = { "actions.select", opts = { tab = true } },
-			-- ["<C-p>"] = "actions.preview",
-			-- ["<C-c>"] = { "actions.close", mode = "n" },
-			-- ["<C-l>"] = "actions.refresh",
-			-- ["-"] = { "actions.parent", mode = "n" },
-			-- ["_"] = { "actions.open_cwd", mode = "n" },
-			-- ["`"] = { "actions.cd", mode = "n" },
-			-- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-			-- ["gs"] = { "actions.change_sort", mode = "n" },
-			-- ["gx"] = "actions.open_external",
-			-- ["g."] = { "actions.toggle_hidden", mode = "n" },
-			-- ["g\\"] = { "actions.toggle_trash", mode = "n" },
-		},
-	},
-	keys = {
-		{
-			"<leader>o",
-			function()
-				local oil = require("oil")
-				oil.open_float(nil, {
-					preview = {
-						vertical = is_vsplit() and true or false,
-					},
-				})
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<S-h>"] = { "actions.parent", mode = "n" },
+      ["<S-l>"] = "actions.select",
+      ["<CR>"] = "actions.select",
+      ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      -- ["<C-l>"] = "actions.refresh",
+      -- ["-"] = { "actions.parent", mode = "n" },
+      -- ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    use_default_keymaps = false
+  },
+  keys = {
+    {
+      "<leader>o",
+      function()
+        local oil = require("oil")
+        oil.open_float(nil, {
+          preview = {
+            vertical = is_vsplit() and true or false,
+          },
+        })
 
-				-- Wait until oil has opened, for a maximum of 1 second.
-				vim.wait(1000, function()
-					return oil.get_cursor_entry() ~= nil
-				end)
-			end,
-			desc = "Open File Explorer",
-		},
-	},
+        -- Wait until oil has opened, for a maximum of 1 second.
+        vim.wait(1000, function()
+          return oil.get_cursor_entry() ~= nil
+        end)
+      end,
+      desc = "Open File Explorer",
+    },
+  },
 }
