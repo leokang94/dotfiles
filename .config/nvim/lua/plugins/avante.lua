ebf826bd2e220da98b51f7b2fd6d4e2137adecbe
leokang94 <acccdang8659@gmail.com>
2025-03-11 11:34:00 +0900

    imp(nvim): update avanten.nvim config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,63 +1,74 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = true,
-  version = false, -- set this if you want to always pull the latest change
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons",
-      },
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-  opts = {
-    provider = "claude",
-    behaviour = {
-      auto_suggestions = false, -- Experimental stage
-      auto_set_highlight_group = true,
-      auto_set_keymaps = true,
-      auto_apply_diff_after_generation = true,
-      support_paste_from_clipboard = true,
-    },
-    file_selector = {
-      provider = "fzf",
-      provider_opts = {},
-    },
-    windows = {
-      width = 35,
-      position = "smart",
-    },
-  },
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = true,
+	version = false, -- set this if you want to always pull the latest change
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					-- required for Windows users
+					use_absolute_path = true,
+				},
+			},
+		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+				"nvim-tree/nvim-web-devicons",
+			},
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+	opts = {
+		provider = "claude",
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+			auto_set_highlight_group = true,
+			auto_set_keymaps = true,
+			auto_apply_diff_after_generation = true,
+			support_paste_from_clipboard = true,
+		},
+		file_selector = {
+			provider = "fzf",
+			provider_opts = {},
+		},
+		mappings = {
+			sidebar = {
+				close = { "q", "<Esc>", "<C-c>" },
+				close_from_input = { normal = { "q", "<Esc>", "<C-c>" }, insert = { "<C-c>" } },
+			},
+		},
+		windows = {
+			width = 50,
+			height = 30,
+			position = "smart",
+
+			ask = {
+				start_insert = false,
+			},
+		},
+	},
 }
