cc0f10e510dfe8e6e5c03cacae8e56e8a5e2027e
leokang94 <acccdang8659@gmail.com>
2025-04-15 10:58:52 +0900

    imp(nvim): update mcp settings

--- .config/nvim/lua/plugins/mcphub.lua
+++ .config/nvim/lua/plugins/mcphub.lua
@@ -3,8 +3,10 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
 	},
-	-- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
+	cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
 	build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+	-- build = "bundled_build.lua",
+	-- use_bundleed_binary = true,
 	opts = {
 		-- Required options
 		port = 9999, -- Port for MCP Hub server
