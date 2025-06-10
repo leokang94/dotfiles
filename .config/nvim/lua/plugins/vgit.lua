af68b4a5947ab6cc131ab055d3eb732ee831d422
leokang94 <acccdang8659@gmail.com>
2025-06-10 14:28:20 +0900

    imp(nvim): change git hunk plugin (gitsign -> vgit)

---
+++ .config/nvim/lua/plugins/vgit.lua
@@ -0,0 +1,79 @@
+return {
+	"tanvirtin/vgit.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+	-- Lazy loading on 'VimEnter' event is necessary.
+	event = "VimEnter",
+	keys = {
+		{
+			"[h",
+			function()
+				require("vgit").hunk_down()
+			end,
+			desc = "Previous Hunk",
+		},
+		{
+			"]h",
+			function()
+				require("vgit").hunk_up()
+			end,
+			desc = "Next Hunk",
+		},
+		{
+			"<leader>ghs",
+			function()
+				require("vgit").buffer_hunk_stage()
+			end,
+			desc = "Stage Hunk",
+		},
+		{
+			"<leader>ghr",
+			function()
+				require("vgit").buffer_hunk_reset()
+			end,
+			desc = "Reset Hunk",
+		},
+		{
+			"<leader>gbs",
+			function()
+				require("vgit").buffer_stage()
+			end,
+			desc = "Stage Buffer",
+		},
+		{
+			"<leader>gbu",
+			function()
+				require("vgit").buffer_unstage()
+			end,
+			desc = "Unstage Buffer",
+		},
+		{
+			"<leader>gbr",
+			function()
+				require("vgit").buffer_reset()
+			end,
+			desc = "Reset Buffer",
+		},
+		{
+			"<leader>ghp",
+			function()
+				require("vgit").buffer_hunk_preview()
+			end,
+			desc = "Preview Hunk",
+		},
+		{
+			"<leader>gbp",
+			function()
+				require("vgit").buffer_blame_preview()
+			end,
+			desc = "Preview Blame",
+		},
+		{
+			"<leader>gdp",
+			function()
+				require("vgit").buffer_diff_preview()
+			end,
+			desc = "Preview Buffer Diff",
+		},
+	},
+	opts = {},
+}
