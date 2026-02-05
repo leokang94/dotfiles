50fc6dd35e8448076db18790f6d9f4006e2fe00d
leokang94 <acccdang8659@gmail.com>
2026-02-05 18:26:39 +0900

    feat(bootstrap, .config): remove aerospace and add flashspace

--- bootstrap.sh
+++ bootstrap.sh
@@ -71,13 +71,13 @@ brew install --force --cask \
   spotify \
   obsidian \
   karabiner-elements \
-  nikitabobko/tap/aerospace \
+  flashspace \
   homerow \
   mouseless \
   1password \
   1password-cli \
-  sf-symbols
-echo "${LEO_PREFIX} Download ${CIAN}programs${CLEAR} from homebrew... ${DONE_POSTFIX}"
+  sf-symbols \
+  echo "${LEO_PREFIX} Download ${CIAN}programs${CLEAR} from homebrew... ${DONE_POSTFIX}" # nikitabobko/tap/aerospace \
 
 # services start
 brew services restart borders
@@ -144,6 +144,7 @@ create_symlink --type=multiple "$HOME/${DOT_FILES_DIR_NAME}/.config" "$HOME/${CO
 create_symlink --type=single "$HOME/${DOT_FILES_DIR_NAME}/.git-custom-commands" "$HOME"
 create_symlink --type=multiple "$HOME/${DOT_FILES_DIR_NAME}/.claude" "$HOME/.claude"
 create_symlink --type=single "$HOME/${DOT_FILES_DIR_NAME}/.config/mouseless/config.yaml" "$HOME/Library/Containers/net.sonuscape.mouseless/Data/.mouseless/configs/config.yaml"
+create_symlink --type=single "$HOME/${DOT_FILES_DIR_NAME}/.config/flashspace/settings.json" "$HOME/${CONFIG_DIR_NAME}/flashspace/settings.json"
 
 ##########################################################
 # Extends my .gitconfig to ~/.gitconfig
